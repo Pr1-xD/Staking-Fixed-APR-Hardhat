d278a7f7512e92bd74a0aab42152bb6bbda67011
Pr1-xD <63440566+Pr1-xD@users.noreply.github.com>
2023-01-24 16:25:18 +0530

    Changed Config

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,7 +1,21 @@
+require('dotenv').config();
+require("@nomiclabs/hardhat-ethers");
+require("@nomiclabs/hardhat-etherscan");
 require("@nomicfoundation/hardhat-toolbox");
 
-/** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
+  defaultNetwork: "hardhat",
+  networks: {
+    hardhat: {
+    },
+    polygon_mumbai: {
+      url: "https://rpc-mumbai.maticvigil.com",
+      accounts: [process.env.PRIVATE_KEY]
+    }
+  },
+  etherscan: {
+    apiKey: process.env.POLYGONSCAN_API_KEY
+  },
   solidity: {
     compilers: [
       {
@@ -22,4 +36,6 @@ module.exports = {
       }
       }
   ]
-}};
+},
+}
+
